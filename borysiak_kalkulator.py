8a31ae341052dc4d63ad9f852d5f8d03c089e77f
Mateusz <mate.borysiak@gmail.com>
2021-08-24 23:13:59 +0200

    Mój pierwszy kalkulator w Python

---
+++ borysiak_kalkulator.py
@@ -0,0 +1,27 @@
+#czesc to moj program z kalkulatorem
+
+print("Witam Cię w moim pierwszym kalkulatorze w języku Python")
+
+a = float(input("Podaj pierwszą literę: "))
+b = float(input("Podaj drugą literę: "))
+
+print(
+"""Wybierz operację na wskazanych liczbach:
+d - dodawanie;
+o - odejmowanie;
+m - mnożenie;
+d - dzielenie"""
+)
+
+c = str(input("Wybrana operacja: "))
+
+if c == 'd':
+    print(a+b)
+elif c == 'o':
+    print(a-b)
+elif c =='m':
+    print(a*b)
+elif c =='d':
+    print(a/b)
+else:
+    print("Wprowadź poprawną operację!")
